74914eaf55cb3f5e67464da1f900e64f2fa299dc
Dpark2021 <37359500+Dpark2021@users.noreply.github.com>
2018-04-25 10:58:21 -0500

    Update Background.h

--- Background.h
+++ Background.h
@@ -1,3 +1,12 @@
+/*
+* Author: David Park, Isaias Lozano, Cheyenne Zhang, Chengyu Hong
+* Assignment Title:  Galaga
+* Assignment Description: This program mimics the game Galaga.
+* Due Date: 4/25/2018
+* Date Created: 3/25/2018
+* Date Last Modified: 4/25/2018
+ */
+
 #ifndef BACKGROUND_H_INCLUDED
 #define BACKGROUND_H_INCLUDED
 
